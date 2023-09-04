c372a6156c1f27316c2829647a2bfb2b1271949f
Travis Truttschel <ttruttschel@istreamfs.com>
2023-09-04 02:35:46 -0500

    Argon2 encoded hash buffer is now significantly more accurate. Reworked options to make variable assignment a bit more flexible.

--- Source/Argon2.Interop/Argon2.cs
+++ Source/Argon2.Interop/Argon2.cs
@@ -30,17 +30,19 @@ public class Argon2
     public string Hash(byte[] password, byte[] salt)
     {
         var hash = new byte[_options.HashLength];
-        var encoded = new byte[39 + (password.Length + salt.Length) * 4];
+        var expectedArgonTemplate = $"$argon2{_options.Type}$v={(uint)_options.Version}$m={_options.MemoryCost},t={_options.TimeCost},p={_options.Parallelism}$$";
+        // We are able to determine our expected hash length, however Argon2 requires the encoded array to be padded with at least 1 extra byte. We remove it later on.
+        var encoded = new byte[expectedArgonTemplate.Length + Base645Len(_options.HashLength) + Base645Len(salt.Length) + 1];
         var error = argon2_hash(
-            timeCost: _options.TimeCost, 
-            memoryCost: _options.MemoryCost,
-            parallelism: _options.Parallelism, 
+            timeCost: (uint) _options.TimeCost, 
+            memoryCost: (uint) _options.MemoryCost,
+            parallelism: (uint) _options.Parallelism, 
             password: password, 
             passwordLength: (nuint) password.Length, 
             salt: salt, 
             saltLength: (nuint) salt.Length,
             hash: hash, 
-            hashLength: _options.HashLength, 
+            hashLength: (nuint) _options.HashLength, 
             encoded: encoded,
             encodedLength: (nuint) encoded.Length, 
             type: (uint) _options.Type, 
@@ -50,6 +52,9 @@ public class Argon2
         {
             throw new CryptographicException($"Failed to generate Argon2 hash. (Error: {error})");
         }
+        
+        // Remove padding.
+        encoded = encoded.Where(x => x != 0x00).ToArray();
 
         return Encoding.ASCII.GetString(encoded);
     }
@@ -67,5 +72,10 @@ public class Argon2
         rng.GetBytes(salt);
 
         return salt;
+    }
+    
+    private static int Base645Len(int length)
+    {
+        return (int) Math.Ceiling((double)length / 3 * 4);
     }
 }
--- Source/Argon2.Interop/Argon2Options.cs
+++ Source/Argon2.Interop/Argon2Options.cs
@@ -2,13 +2,13 @@
 
 public class Argon2Options
 {
-    public uint TimeCost { get; init; } = 12;
+    public int TimeCost { get; init; } = 12;
 
-    public uint MemoryCost { get; init; } = 1024 * 64;
+    public int MemoryCost { get; init; } = 1024 * 64;
 
-    public uint Parallelism { get; init; } = (uint) Environment.ProcessorCount * 2;
+    public int Parallelism { get; init; } = Environment.ProcessorCount * 2;
 
-    public nuint HashLength { get; init; } = 32;
+    public int HashLength { get; init; } = 32;
 
     public Argon2Type Type { get; init; } = Argon2Type.ID;
     public Argon2Version Version { get; init; } = Argon2Version.Nineteen;
